8ba8837f7a8a8d771e6469efcfdd9b3980ee885a
gljames24 <gljames24@gmail.com>
2023-03-27 22:54:59 -0600

    Finished Android Basic Calculator

--- app/src/main/java/com/example/basiccalculator/MainActivity.kt
+++ app/src/main/java/com/example/basiccalculator/MainActivity.kt
@@ -84,7 +84,7 @@ class MainActivity : AppCompatActivity() {
                     }
                 }
                 SymbolType.ARITHMETIC_SYMBOL -> {
-                    if((!hasOperator || hasExponent) && !(binding.equationTextView.text=="_")){
+                    if((!hasOperator || hasExponent) && !(binding.equationTextView.text.contains("_"))){
                         binding.equationTextView.append(view.text)
                         hasOperator = true
                         hasDecimal = false
@@ -110,7 +110,7 @@ class MainActivity : AppCompatActivity() {
                     else if(hasOperator && !hasExponent){
                         binding.equationTextView.append("-")
                     }
-                    else if(binding.equationTextView.text=="_"){
+                    else if(binding.equationTextView.text.contains("_")){
                         binding.equationTextView.text="-"
                     }
                 }
@@ -126,14 +126,14 @@ class MainActivity : AppCompatActivity() {
     fun invertSignAction(view: View) {
         if(binding.equationTextView.text.last().equals("-")){
             binding.equationTextView.text = binding.equationTextView.text.subSequence(0, binding.equationTextView.length() - 1)
-            if(binding.equationTextView.text==""){
+            if(binding.equationTextView.text.equals("")){
                 binding.equationTextView.text="_"
             }
         }
         else if(hasOperator && !hasExponent){
             binding.equationTextView.append("-")
         }
-        else if(binding.equationTextView.text=="_"){
+        else if(binding.equationTextView.text.equals("_")){
             binding.equationTextView.text="-"
         }
     }
@@ -187,26 +187,21 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun calculateResult(): String {
-        return parsedEquation(binding.equationTextView.text).toString()
+        return binding.equationTextView.text.parseEquation().parseExponent().parseMultDiv().parseAddSub()[0]
     }
 
-//    private fun exponentiate(parsedEquation: MutableList<Any>): Any {
-//
-//    }
-
-
-    private fun parsedEquation(input : CharSequence): MutableList<String>{
+    private fun CharSequence.parseEquation(): MutableList<String>{
         val output = mutableListOf<String>()
         var lastEndIndex = 0
-        for (i in input.indices) {
-            val c = input[i]
+        for (i in this.indices) {
+            val c = this[i]
             if (c in "-.0123456789") {
-                if (i == input.lastIndex || input[i+1] !in ".0123456789") {
-                    val subsequence = input.subSequence(lastEndIndex, i+1).toString()
+                if (i == this.lastIndex || this[i+1] !in ".0123456789") {
+                    val subsequence = this.subSequence(lastEndIndex, i+1).toString()
                     output.add(subsequence)
                     lastEndIndex = i + 1
                 }
-            } else if (c in "+×÷—") {
+            } else if (c in "+×÷–") {
                 output.add(c.toString())
                 lastEndIndex = i + 1
             } else if (c in "⁰¹²³⁴⁵⁶⁷⁸⁹") {
@@ -217,20 +212,90 @@ class MainActivity : AppCompatActivity() {
         return output
     }
 
-    private fun superscriptToDigit(superscript: Char): Char {
-        return when (superscript) {
-            '⁰' -> '0'
-            '¹' -> '1'
-            '²' -> '2'
-            '³' -> '3'
-            '⁴' -> '4'
-            '⁵' -> '5'
-            '⁶' -> '6'
-            '⁷' -> '7'
-            '⁸' -> '8'
-            '⁹' -> '9'
-            else -> throw IllegalArgumentException("Invalid superscript character: $superscript")
+    fun MutableList<String>.parseExponent(): MutableList<String> {
+        val result = mutableListOf<String>()
+        var i = 0
+        while (i < this.size) {
+            if (i + 1 < this.size && this[i + 1].isSuperscriptDigit()) {
+                val numberString = this[i]
+                val superscriptChar = this[i + 1][0]
+                val base = numberString.toDoubleOrNull() ?: 0.0
+                val exponent = superscriptToDigit(superscriptChar).toString().toDoubleOrNull() ?: 0.0
+                val powResult = Math.pow(base, exponent).toString()
+                result.add(powResult)
+                i += 2
+            } else {
+                result.add(this[i])
+                i++
+            }
         }
+        return result
+    }
+
+    fun MutableList<String>.parseMultDiv(): MutableList<String> {
+        val result = mutableListOf<String>()
+        var i = 0
+        while (i < this.size) {
+            val token = this[i]
+            if (token == "×" || token == "÷") {
+                val left = result.removeLast().toDouble()
+                val right = this[i + 1].toDouble()
+                val value = when (token) {
+                    "×" -> left * right
+                    "÷" -> left / right
+                    else -> throw IllegalArgumentException("Unknown operator: $token")
+                }
+                result.add(value.toString())
+                i += 2
+            } else {
+                result.add(token)
+                i++
+            }
+        }
+        return result
+    }
+
+    fun MutableList<String>.parseAddSub(): MutableList<String> {
+        val result = mutableListOf<String>()
+        var i = 0
+        while (i < this.size) {
+            val token = this[i]
+            if (token == "+" || token == "–") {
+                val left = result.removeLast().toDouble()
+                val right = this[i + 1].toDouble()
+                val value = when (token) {
+                    "+" -> left + right
+                    "–" -> left - right
+                    else -> throw IllegalArgumentException("Unknown operator: $token")
+                }
+                result.add(value.toString())
+                i += 2
+            } else {
+                result.add(token)
+                i++
+            }
+        }
+        return result
+    }
+
+    private fun superscriptToDigit(superscript: Char): Double {
+        return when (superscript) {
+            '⁰' -> 0.0
+            '¹' -> 1.0
+            '²' -> 2.0
+            '³' -> 3.0
+            '⁴' -> 4.0
+            '⁵' -> 5.0
+            '⁶' -> 6.0
+            '⁷' -> 7.0
+            '⁸' -> 8.0
+            '⁹' -> 9.0
+            else -> -1.0
+        }
+    }
+
+    fun String.isSuperscriptDigit(): Boolean {
+        return superscriptToDigit(this[0])!=-1.0
     }
 
 }
